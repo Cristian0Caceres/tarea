7aa66b5c7ba41d600370ad6191d6918a4d892164
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-09-06 13:43:39 -0300

    Update t00 copy.py

--- t00 copy.py
+++ t00 copy.py
@@ -3,29 +3,44 @@ class SistemaDeReservas:
     def __init__(self,filas,columnas):
         self.filas=filas
         self.columnas=columnas
-        filas= self.filas
-        columnas= self.columnas
-        matriz = [[ __ for __ in range(columnas)] for __ in range(filas)]  
-        for y in range(columnas):
-            for x in range(filas):
-                matriz[x][y] = 'N'
-                vector = np.array(matriz)
-                
-        print(vector) 
+        self.matriz = [[ __ for __ in range(self.columnas)] for __ in range(self.filas)]  
+        for y in range(self.columnas):
+            for x in range(self.filas):
+                 self.matriz[x][y] = 'Dis'
+    def MuestraEstadoCine(self):         
+        vector = np.array(cine.matriz)
+        print(vector)        
+    
 
     def reservar_asiento(self,fila,columna):
-        
+        posx=fila
+        posy=columna
+        cine.matriz[posx][posy] = "res"
+        return "True"
+    def cancelar_reserva(self,fila,columna):
+        posx=fila
+        posy=columna
+        cine.matriz[posx][posy] = "Dis"
+        return "True"
+    def asientos_disponibles(self):
+        disp=0
+        for x in range(0, self.columnas):
+            for y in range(0,self.filas):
+                if cine.matriz[x][y]=="dis":
+                    disp=disp+1
+
+        return disp
 if __name__ == "__main__":
     cine = SistemaDeReservas(3, 7)
     print("Estado inicial del cine:")
-    print(cine)
+    print(cine.MuestraEstadoCine())
     resultado1 = cine.reservar_asiento(2, 3)
     print("Reserva exitosa:", resultado1)
     print("Estado del cine después de la reserva:")
-    print(cine)
+    print(cine.MuestraEstadoCine())
     resultado2 = cine.cancelar_reserva(2, 3)
     print("Cancelación exitosa:", resultado2)
     print("Estado del cine después de la cancelación:")
-    print(cine)
+    print(cine.MuestraEstadoCine())
     disponibles = cine.asientos_disponibles()
     print("Asientos disponibles:", disponibles)
